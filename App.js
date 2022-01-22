6196339d65a811e5dca3c5758486e2b222c1986e
momagan <momagan@gmail.com>
2022-01-21 20:47:36 -0700

    feat: :lipstick: Display posts in a FlatList

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import React from 'react';
 import {StatusBar, SafeAreaView} from 'react-native';
 import Post from './src/components/post';
 import HomeScreen from './src/screens/home';
+import SearchResultsScreen from './src/screens/SearchResults';
 
 import feed from './assets/data/feed';
 
@@ -18,7 +19,8 @@ const App: () => React$Node = () => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         {/* <HomeScreen /> */}
-        <Post post={post1} />
+        {/* <Post post={post1} /> */}
+        <SearchResultsScreen />
       </SafeAreaView>
     </>
   );
