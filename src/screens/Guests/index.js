7b099d7e7afe12315a99971b1c228dfc35772810
momagan <momagan@gmail.com>
2022-01-27 22:13:36 -0700

    feat: :sparkles: Guest Screen

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const GuestsScreen = () => {
+import styles from './styles.js';
+
+const GuestsScreen = props => {
   return (
     <View>
-      <Text>Hello from guest page</Text>
+      <View>
+        <View style={styles.row}>
+          <View>
+            <Text style={{fontWeight: 'bold'}}>Adults</Text>
+            <Text style={{color: '#8d8d8d'}}>Ages 13 or above</Text>
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text style={{marginHorizontal: 20, fontSize: 16}}></Text>
+          </View>
+        </View>
+
+        <View style={styles.row}>
+          <View>
+            <Text style={{fontWeight: 'bold'}}>Children</Text>
+            <Text style={{color: '#8d8d8d'}}>Ages 2-12</Text>
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text style={{marginHorizontal: 20, fontSize: 16}}></Text>
+          </View>
+        </View>
+
+        <View style={styles.row}>
+          <View>
+            <Text style={{fontWeight: 'bold'}}>Infants</Text>
+            <Text style={{color: '#8d8d8d'}}>Under 2</Text>
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text style={{marginHorizontal: 20, fontSize: 16}}></Text>
+          </View>
+        </View>
+      </View>
+
+      <Text style={{fontSize: 20, color: 'white', fontWeight: 'bold'}}>
+        Search
+      </Text>
     </View>
   );
 };
